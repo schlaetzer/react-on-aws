4ab79d5ff59f4675d36fe0b0e4cc03830035a99b
Thorben Schlätzer <thorbenschlaetzer93@gmail.com>
2020-05-06 09:29:47 +0200

    added lambda function to automatically build the app

--- upload-schlaetzer-lambda.py
+++ upload-schlaetzer-lambda.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import json
 import boto3
 import zipfile
 import io
 
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    
+    # Buckets
+    build_bucket = s3.Bucket('build.schlaetzer.com')
+    schlaetzer_bucket = s3.Bucket('schlaetzer.com')
+    
+    # download zip from s3
+    build_zip = io.BytesIO()
+    build_bucket.download_fileobj('buildschlaetzer.zip', build_zip)
+    
+    # upload each of the files to the other s3
+    with zipfile.ZipFile(build_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            schlaetzer_bucket.upload_fileobj(obj, nm)
+            schlaetzer_bucket.Object(nm).Acl().put(ACL='public-read')
 
-
-# Buckets
-build_bucket = s3.Bucket('build.schlaetzer.com')
-schlaetzer_bucket = s3.Bucket('schlaetzer.com')
-
-# download zip from s3
-build_zip = io.BytesIO()
-build_bucket.download_fileobj('buildschlaetzer.zip', build_zip)
-
-# upload each of the files to the other s3
-with zipfile.ZipFile(build_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        schlaetzer_bucket.upload_fileobj(obj, nm)
-        schlaetzer_bucket.Object(nm).Acl().put(ACL='public-read')
+    return {
+        'statusCode': 200,
+        'body': json.dumps('worked')
+    }
