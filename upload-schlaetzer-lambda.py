a60ebb005d229c5c7420397aabdb00107eb641c4
Thorben Schlätzer <thorbenschlaetzer93@gmail.com>
2020-05-06 11:33:19 +0200

    preparing for CodePipeline

--- upload-schlaetzer-lambda.py
+++ upload-schlaetzer-lambda.py
@@ -14,16 +14,32 @@ def lambda_handler(event, context):
     topic = sns.Topic('arn:aws:sns:eu-central-1:948929446452:deploySchlaetzerWebsite')
     
     
+    location={
+        "bucketName": "build.schlaetzer.com",
+        "objectKey": "buildschlaetzer.zip"
+    }
+    
     try:
+        # get the build job
+        job = event.get("CodePipeline.job")
+        
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "BuildArtifact":
+                    location = artifact["location"]["s3Location"]
+                    
+        print("Building portfolio from "+str(location))
+        
+        # define s3
         s3 = boto3.resource('s3')
         
         # Buckets
-        build_bucket = s3.Bucket('build.schlaetzer.com')
+        build_bucket = s3.Bucket(location["bucketName"])
         schlaetzer_bucket = s3.Bucket('schlaetzer.com')
         
         # download zip from s3
         build_zip = io.BytesIO()
-        build_bucket.download_fileobj('buildschlaetzer.zip', build_zip)
+        build_bucket.download_fileobj(location["objectKey"], build_zip)
         
         # upload each of the files to the other s3
         with zipfile.ZipFile(build_zip) as myzip:
@@ -31,6 +47,12 @@ def lambda_handler(event, context):
                 obj = myzip.open(nm)
                 schlaetzer_bucket.upload_fileobj(obj, nm)
                 schlaetzer_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        # tell codepipeline that we have been successful with deploying
+        if job:
+            print("sending pipeline info: "+job["id"])
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject='Error when deploying Schlaetzer.com', Message='Website couldn\'t be successfully deployed...')
         raise
