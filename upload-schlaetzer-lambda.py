3014c2e38c66d1015d5dfc2fb9dcac1a31595f94
Thorben Schlätzer <thorbenschlaetzer93@gmail.com>
2020-05-06 10:24:22 +0200

    adding SNS for deployment success/failure

--- upload-schlaetzer-lambda.py
+++ upload-schlaetzer-lambda.py
@@ -8,23 +8,35 @@ import io
 
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
     
-    # Buckets
-    build_bucket = s3.Bucket('build.schlaetzer.com')
-    schlaetzer_bucket = s3.Bucket('schlaetzer.com')
+    # Topic for SNS
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:eu-central-1:948929446452:deploySchlaetzerWebsite')
     
-    # download zip from s3
-    build_zip = io.BytesIO()
-    build_bucket.download_fileobj('buildschlaetzer.zip', build_zip)
     
-    # upload each of the files to the other s3
-    with zipfile.ZipFile(build_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            schlaetzer_bucket.upload_fileobj(obj, nm)
-            schlaetzer_bucket.Object(nm).Acl().put(ACL='public-read')
+    try:
+        s3 = boto3.resource('s3')
+        
+        # Buckets
+        build_bucket = s3.Bucket('build.schlaetzer.com')
+        schlaetzer_bucket = s3.Bucket('schlaetzer.com')
+        
+        # download zip from s3
+        build_zip = io.BytesIO()
+        build_bucket.download_fileobj('buildschlaetzer.zip', build_zip)
+        
+        # upload each of the files to the other s3
+        with zipfile.ZipFile(build_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                schlaetzer_bucket.upload_fileobj(obj, nm)
+                schlaetzer_bucket.Object(nm).Acl().put(ACL='public-read')
+    except:
+        topic.publish(Subject='Error when deploying Schlaetzer.com', Message='Website couldn\'t be successfully deployed...')
+        raise
 
+    topic.publish(Subject='Schlaetzer.com has been deployed', Message='Website has been successfully deployed')
+    
     return {
         'statusCode': 200,
         'body': json.dumps('worked')
